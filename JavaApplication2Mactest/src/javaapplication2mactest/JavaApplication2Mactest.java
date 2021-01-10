c6bc5331c36806ac6b282a3c41303a5b92a0d08b
Chawapon <chawapon2001@gmail.com>
2021-01-10 19:54:20 +0700

    test mac

--- JavaApplication2Mactest/src/javaapplication2mactest/JavaApplication2Mactest.java
+++ JavaApplication2Mactest/src/javaapplication2mactest/JavaApplication2Mactest.java
@@ -16,7 +16,7 @@ public class JavaApplication2Mactest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("test mac");
+        System.out.println("test mac111");
     }
     
 }
